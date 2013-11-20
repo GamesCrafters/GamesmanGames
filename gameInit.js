8af944569097999cf1a8a5bc0897e34f5979cbc9
Allison Hall <allydhall@berkeley.edu>
2013-11-20 11:09:38 -0800

    Add xml parsing

--- gameInit.js
+++ gameInit.js
@@ -1,4 +1,19 @@
 function Game() {
 }
 
+
 var gamesList = {};
+
+$(document).ready(function() {
+	$.get('xml/games/games.xml',function(data) { 
+		$(data).find('game').each(function() {
+			var $game = $(this);
+			var title = $game.find('title').text(),
+				description = $game.find('description').text(),
+				asset = $game.find('asset').text();
+			
+			gamesList[asset] = [title, description];
+			console.log(gameList);
+		});
+	});
+});
