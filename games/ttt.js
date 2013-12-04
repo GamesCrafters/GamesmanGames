29b3bdffcd456d445683526647fa5f14d2f7e281
Allison Hall <allydhall@berkeley.edu>
2013-12-04 08:24:43 -0800

    Revert "readding features"
    This reverts commit ee5f72821820e41e7029a2745bcb66b9a060115f.

--- games/ttt.js
+++ games/ttt.js
@@ -92,10 +92,9 @@ window.game.notifier = notifier =
           for (i = 0, len = data.length; i < len; i++) {
                move = data[i];
                window.moves[move.move] = move;
-               color = "#OOO";
+               color = "#000";
                if (game.showValueMoves) {
-                  color = game.getColor(move, data);
-                  //console.log(color);
+
                }
                column = row = 0;
                if (game.isC()) {
@@ -112,15 +111,7 @@ window.game.notifier = notifier =
                game.notifier.canvas.fillRect(xpos, ypos, x_pixels, y_pixels);
                game.notifier.canvas.strokeStyle='#000';
                game.notifier.canvas.strokeRect(xpos, ypos, x_pixels, y_pixels);
-              // if (game.showValueMoves) {
-              //     results.push(this.canvas.beginPath());
-              //     results.push(this.canvas.arc(xpos + (x_pixels / 2), ypos + (y_pixels / 2), (x_pixels / 2) - change, 0, Math.PI * 2, false));
-              //     results.push(this.canvas.closePath());
-              //     results.push(this.canvas.fillStyle=color);
-              //     results.push(this.canvas.fill());
-              // } else {
-              //  results.push(void 0);
-              // }
+
                
           }
 
