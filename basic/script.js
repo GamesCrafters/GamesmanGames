5f32ad0059eda1dc8d2d2247da65a679e7beb482
Kyle R. Zentner <zentner.kyle@gmail.com>
2014-10-06 11:25:44 -0700

    Basic othello drawing works.

--- basic/script.js
+++ basic/script.js
@@ -1,5 +1,7 @@
 $(function () {
+  drawBoard(Snap('#main-svg'), '');
   refresh();
+  //$('#game-input').val('quickchess');
   $('#game-input').on('input', refresh);
   $('#position').on('input', function () {
     drawBoard(Snap('#main-svg'), $('#position').val());
@@ -21,5 +23,47 @@ function getStart(game, callback) {
 }
 
 function drawBoard (svg, boardString) {
+  $('#main-svg').html('');
   console.log('drawing', boardString);
+  console.log(svg);
+  for (var x = 0; x < 4; x++) {
+    for (var y = 0; y < 4; y++) {
+      var r = svg.rect(x * 108, y * 108, 100, 100);
+      r.attr({
+        'fill': 'grey'
+      });
+      var c = boardString[x + y * 4];
+      var s;
+      if (c === 'B') {
+        s = svg.circle(x * 108 + 50, y * 108 + 50, 40);
+        s.attr({
+          'fill': 'black'
+        });
+      }
+      if (c === 'W') {
+        s = svg.circle(x * 108 + 50, y * 108 + 50, 40);
+        s.attr({
+          'fill': 'white'
+        });
+      }
+      if (s) {
+        s.click(function () {
+          s.selected = !s.selected;
+          if (s.selected) {
+            this.attr({
+              'r': 50,
+            });
+          } else {
+            this.attr({
+              'r': 40,
+            });
+          }
+        });
+      }
+      console.log(c)
+    }
+  }
+  //svg.attr({
+    //'transform': 'scale(1.2)'
+  //});
 }
